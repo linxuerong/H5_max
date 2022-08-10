294eee0256632cc34b2cbe7297e843c3f33ba620
wjs <876827244@qq.com>
2022-08-10 17:57:10 +0800

    时间

--- js/main.js
+++ js/main.js
@@ -25,3 +25,24 @@ window.addEventListener('load', function() {
 		window.innerWidth > 576 && investmentListHide()
 	})
 })
+
+function getNow(s) {
+	return s < 10 ? '0' + s: s;
+}
+
+$(function (){
+	setInterval( ()=>{
+		console.log("EEE")
+		var myDate = new Date();
+		var year=myDate.getFullYear();        //获取当前年
+		var month=myDate.getMonth()+1;   //获取当前月
+		var date=myDate.getDate();            //获取当前日
+		var h=myDate.getHours();              //获取当前小时数(0-23)
+		var m=myDate.getMinutes();          //获取当前分钟数(0-59)
+		var s=myDate.getSeconds();
+		var now=year+'-'+getNow(month)+"-"+getNow(date)+" "+getNow(h)+':'+getNow(m)+":"+getNow(s);
+		$(".header-time").text(now)
+	},1000)
+})
+
+
